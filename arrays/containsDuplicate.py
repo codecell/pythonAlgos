191734edccb32c28e5d8f9c5fef3332b9167184f
Ezaka Alfred <ezakaphil@gmail.com>
2021-07-08 19:18:31 +0100

    Add solution 2 for Array  containsDuplicate

--- arrays/containsDuplicate.py
+++ arrays/containsDuplicate.py
@@ -1,19 +1,39 @@
 # Q. https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
 
-def containsDuplicate(nums): 
+from collections import defaultdict
+
+
+def containsDuplicate(nums):
     if len(nums) <= 1:
         return False
-
-    i = 0
-    k = len(nums) - 1
-
-    while i + 1 <= k:
-        if nums[i] == nums[k]:
+    
+    d = defaultdict(int)
+    
+    for k in nums:
+        d[k] += 1
+        
+    for key in d:
+        if d[key] >= 2:
             return True
 
-        i += 1
-        k -= 1
+    return False 
+# Solution 2
+# def containsDuplicate(nums): 
+#     if len(nums) <= 1:
+#         return False
 
-    return False    
+#     i = 0
+#     k = len(nums) - 1
+
+#     while i + 1 <= k:
+#         if nums[i] == nums[k]:
+#             return True
+
+#         i += 1
+#         k -= 1
+
+#     return False    
 
 print(containsDuplicate([1, 2, 3, 4]))
+print(containsDuplicate([1, 2, 3, 1]))
+print(containsDuplicate([1, 2, 2, 4]))
