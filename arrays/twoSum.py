25a3848ceacf97477c0157be4e029f7d229b3cdf
Ezaka Alfred <ezakaphil@gmail.com>
2021-07-10 18:14:58 +0100

    Add solution for Arrays twoSum

---
+++ arrays/twoSum.py
@@ -0,0 +1,14 @@
+# Q. https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546/
+
+def twoSum(nums, target):
+    currentIndez = 0
+
+    lent = len(nums)
+
+    while currentIndez < lent:
+        for k in range(currentIndez + 1, lent):
+            if nums[currentIndez] + nums[k] == target and currentIndez != k:
+                return [currentIndez, k]
+
+
+print(twoSum([2, 2, 6, 1, 3], 5))
