11d72d9fa0f65844a3a472e500d15f121de3e96e
Ezaka Alfred <ezakaphil@gmail.com>
2021-07-10 18:24:18 +0100

    Add solution for Arrays twoSum

--- arrays/twoSum.py
+++ arrays/twoSum.py
@@ -1,5 +1,10 @@
 # Q. https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546/
 
+'''
+    time complexity here is O(n * n) in the worst case
+    The Space complexity is constant here since the whole operation is modified in place and no new arrays are created O(1) 
+'''
+
 def twoSum(nums, target):
     currentIndez = 0
 
