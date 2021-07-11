c347900b488f8d44b0eac3e24a59389df9f52b0b
Ezaka Alfred <ezakaphil@gmail.com>
2021-07-11 14:44:46 +0100

    Add solution for Array plusOne question

---
+++ arrays/plusOne.py
@@ -0,0 +1,18 @@
+# Q. https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/
+
+def plusone(digits):
+    res = []
+
+    arrToStr = ''.join(str(el) for el in digits)
+    arrToStrToInt = int(arrToStr) + 1
+
+    intToStr = str(arrToStrToInt)
+    
+    for item in intToStr:
+        res.append(int(item))
+
+    return res
+
+
+print(plusone([9])) #[1, 0]
+print(plusone([1, 2, 3])) #[1, 2, 3]
