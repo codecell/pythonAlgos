e7c7986ba70274aac35d791c86275c7dc390edcb
Ezaka Alfred <ezakaphil@gmail.com>
2021-07-21 12:56:54 +0100

    Add time complexity of the palindrome solution

--- strings/isPalindrome.py
+++ strings/isPalindrome.py
@@ -1,4 +1,9 @@
 # Q. https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/883/
+
+'''
+    space complexity here is O(1)
+    time is O(n)
+'''
 import re
 
 def isPalindrome(s):
