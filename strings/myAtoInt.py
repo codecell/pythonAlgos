b0ffc46302733deea9289fd470e0eb6f83cfdac0
Ezaka Alfred <ezakaphil@gmail.com>
2021-07-22 18:24:04 +0100

    Add improved solution for myAtoInt problem

--- strings/myAtoInt.py
+++ strings/myAtoInt.py
@@ -1,24 +1,31 @@
 # Q. https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/884/
+    '''
+        space complexity O(n)
+        time complexity O(n)
 
-import string
+        Regex explanation =>
+        ^       --starts with
+        \s*     --with zero or more spaces
+        [-|+]?  --with or without a +ve or -ve sign
+        \d+     --one or more digits
+    '''
+
+import re
 
 def myAtoi(s):
-    st = s.translate(str.maketrans('', '', string.whitespace))
-    lent = len(st)
-    numStr = ''
-
-    for i in range(0, lent):
-        if lent > i + 1 and st[i].isalpha() == True and st[i + 1].isalpha() == False:
-            return 0
-        if st[i].isalpha() == False:
-            numStr += st[i]
-
-    if int(numStr) < (-1 * (2**31)):
-        return (-1 * (2**31))
-    elif int(numStr) > 2**31 - 1:
+    numsList = re.findall('^\s*[-|+]?\d+', s)
+    numString = ''.join(numsList)
+    
+    if numsList == []:
+        return 0
+    
+    if int(numString) < -1 * 2**31:
+        return -1 * 2**31
+    
+    if int(numString) > 2**31 - 1:
         return 2**31 - 1
     
-    return int(numStr)
+    return int(numString)
 
 
 print(myAtoi('42')) # 42
