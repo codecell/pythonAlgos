c5664521a77d270f257faefc643e4b4d09cb525d
Ezaka Alfred <ezakaphil@gmail.com>
2021-08-01 16:13:03 +0100

    Update solution of delete from N positions from the end

--- linkedlist/deleteNthNode.py
+++ linkedlist/deleteNthNode.py
@@ -1,27 +1,27 @@
 # Q.) https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/603/
 
 def removeNthFromEnd(head, n: int):
-    holderList = ListNode()
-    holderList.next = head
-    tracer = head
-    lent = 0
+    holderList = head
+    lent = 1
 
     # To get the length
-    while tracer != None:
-        tracer = tracer.next
+    while head.next:
+        head = head.next
 
         lent += 1
 
-    lent -= n
-    tracer = holderList
+    head = holderList
 
-    while lent > 0:
-        tracer = tracer.next
+    if n == lent:
+        return holderList.next
 
-        lent -= 1
-    
-    tracer.next = tracer.next.next
+    iterations = lent - n - 1
 
-    return holderList.next
+    for i in range(iterations):
+        head = head.next
+
+    head.next = head.next.next
+
+    return holderList
 
 
