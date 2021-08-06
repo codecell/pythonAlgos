c1934735d4baf69c2c891c5a6f6d1c15b384488c
Ezaka Alfred <ezakaphil@gmail.com>
2021-08-06 07:14:39 +0100

    Add solution for reversing a list

---
+++ linkedlist/reverseLinkedList.py
@@ -0,0 +1,23 @@
+# Q. https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/560/
+
+'''
+    Time complxity - O(n)
+    Space complexity - O(1)
+'''
+
+def reverseList(head: ListNode):
+    prev = None
+    current = head
+    
+    while(current is not None):
+        nextNode = current.next
+        current.next = prev
+        prev = current
+        current = nextNode
+        head = prev
+        
+    # print(head)
+    return head
+
+print(reverseList([1, 2, 3, 4, 5])) # [5, 4, 3, 2, 1]
+print(reverseList([9, 8])) # [8, 9]
