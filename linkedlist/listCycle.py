84a4db742efeeea2129d3b9aab2265f91fdde7eb
Ezaka Alfred <ezakaphil@gmail.com>
2021-08-09 13:51:29 +0100

    Add solution for linkedList cycle

---
+++ linkedlist/listCycle.py
@@ -0,0 +1,19 @@
+# Q. https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/773/
+
+def hasCycle(head):
+    if head == None or head.next == None:
+        # means there was a termination
+        return False
+
+    slowPointer, fastPointer = head, head.next
+
+    while slowPointer != fastPointer:
+        if fastPointer == None or fastPointer.next == None:
+            # fastPointer or fastPointer.next will eventually == None if no cycle
+            return False
+
+        slowPointer, fastPointer = slowPointer.next, fastPointer.next.next
+
+    # if it gets here then, fastPointer == fastPointer.next == a point in cycle == loop
+
+    return True
