5651d79fd3ec7e58c7604beb25564f01c54174a2
Ezaka Alfred <ezakaphil@gmail.com>
2021-08-17 19:47:06 +0100

    Add solution for Breath first search(level order) traveral

---
+++ trees/bfs_levelOrder.py
@@ -0,0 +1,26 @@
+# Q. https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/628
+'''
+  time - O(n)
+  space - O(1)
+'''
+def levelOrder(root):
+  h = defaultdict(int)
+
+  def addLevel(node, level):
+    if node:
+      level += 1
+      addLevel(node.left, level)
+      addLevel(node.right, level)
+
+      if h[level]:
+        h[level].append(node.val) #{1: [val]}
+      else:
+        h[level] = [node.val]
+
+  addLevel(root, 0)
+  res = []
+
+  for i in range(1, len(h.keys()) + 1):
+    res.append(h[i])
+
+  return res
